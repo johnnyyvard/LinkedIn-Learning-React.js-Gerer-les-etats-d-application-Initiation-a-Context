c410dc2fff41c2773ed646d0986aa4c1996e6bc6
johnnyyvard <johnnyyvard@me.com>
2021-01-12 22:19:28 +0100

    Finalisation du projet

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import NavBar from './NavBar'
-import ToDoList from './ToDoList'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import NavBar from './NavBar';
+import ToDoList from './ToDoList';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import AddTask from './AddTask';
-// import initialData from '../initialData';
-// import uniqueid from 'uniqueid'
-// import Fetching from './Fetching'
 
 const initialData = {
     tasks: [
@@ -20,94 +17,21 @@ const TaskData = React.createContext();
 export const TaskConsumer = TaskData.Consumer;
 
 class App extends React.Component {
-
-    // state = {
-    //     tasks: [],
-    //     fetching: true
-    // }
-
-    // componentDidMount = () => {
-    //     let delay = Math.floor(Math.random() * 5000)
-
-    //     setTimeout(() => {
-    //         this.setState({
-    //             fetching: false,
-    //             tasks: initialData
-    //         })
-    //     } , delay)
-    // }
-
-    // onToggleCompleted = (taskId) => {
-    //     let taskToUpdate = this.state.tasks.find(task => task.id === taskId)
-    //     taskToUpdate.completed = !taskToUpdate.completed
-
-    //     this.setState(prevState => (
-    //         prevState.tasks.map(task => {
-    //             return task.id === taskId ? taskToUpdate : task
-    //         })
-    //     ))
-    // }
-
-    // onAddTask = (newTaskName) => {
-    //     let newTask = {
-    //         id: uniqueid(),
-    //         name: newTaskName,
-    //         completed: false
-    //     }
-
-    //     this.setState(prevState => ({
-    //         tasks: [...prevState.tasks, newTask]
-    //     }))
-    // }
-
-    // onDeleteCompleted = () => {
-    //     this.setState(prevState => {
-    //         let newState = prevState.tasks.filter(task => !task.completed)
-    //         return {
-    //             tasks: newState
-    //         }
-    //     })
-    // }
-
-    // // shouldComponentUpdate = () => {
-    // //     console.log('Bonjour de shouldComponentUpdate')
-    // //     return true
-    // // }
-
-    // // componentDidMount = () => {
-    // //     console.log('Bonjour de componentDidMount')
-    // // }
-
-    // // componentDidUpdate = () => {
-    // //     console.log('Bonjour de componentDidUpdate')
-    // // }
-
-    // // getSnapshotBeforeUpdate = () => {
-    // //     console.log('Bonjour de getSnapshotBeforeUpdate')
-    // // }
-
-
-
     render() {
-        // console.log('Bonjour de render')
-
         return (
             <TaskData.Provider value={initialData} >
                 <section id="todo">
-                    {/* {this.state.fetching? <Fetching /> : null} */}
                     <BrowserRouter>
                         <Switch>
                             <Route path="/add-task" component={AddTask} />
                             <Route path="/:filter?" component={ToDoList} />
                         </Switch>
                         <NavBar onDeleteCompleted={this.onDeleteCompleted} />
-
                     </BrowserRouter>
                 </section>
-
             </TaskData.Provider>
         )
     }
-}
+};
 
-export default App
+export default App;
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -3,30 +3,6 @@ import ToDo from './ToDo';
 import { TaskConsumer } from "./App";
 
 const ToDoList = () => {
-    // let filteredTasks
-
-    // switch (match.params.filter) {
-    //     case 'completed':
-    //         filteredTasks = tasks.filter(task => task.completed)
-    //         break;
-
-    //     default:
-    //         filteredTasks = tasks
-    //         break;
-    // }
-
-    // if (filteredTasks.length === 0) {
-    //     return (
-    //         <>
-    //             <h1 className="m-3">Liste de tâches</h1>
-    //             <ul className="list-group m-3">
-    //                 <li className="list-group-item">Aucune tâche à afficher.</li>
-    //             </ul>
-    //         </>
-
-    //     )
-
-    // } else {
     return (
         <>
             <h1 className="m-3">Liste de tâches</h1>
@@ -37,15 +13,10 @@ const ToDoList = () => {
                             tasks.map((task) => <ToDo task={task} key={task.id} />)
                         }
                     </ul>
-
                 )}
-
             </TaskConsumer>
         </>
-
     )
-
-    // }
-}
+};
 
 export default ToDoList;
