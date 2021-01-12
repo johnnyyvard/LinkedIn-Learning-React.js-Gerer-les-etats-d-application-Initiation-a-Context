ae66ac759e216f21b24db77e65c02793107118ed
johnnyyvard <johnnyyvard@me.com>
2021-01-12 21:41:05 +0100

    Finalisation du composant

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,44 +1,51 @@
-import React from 'react'
-import ToDo from './ToDo'
+import React from 'react';
+import ToDo from './ToDo';
+import { TaskConsumer } from "./App";
 
-const ToDoList = ({ tasks, match, onToggleCompleted }) => {
-    let filteredTasks
+const ToDoList = () => {
+    // let filteredTasks
 
-    switch (match.params.filter) {
-        case 'completed':
-            filteredTasks = tasks.filter(task => task.completed)
-            break;
+    // switch (match.params.filter) {
+    //     case 'completed':
+    //         filteredTasks = tasks.filter(task => task.completed)
+    //         break;
 
-        default:
-            filteredTasks = tasks
-            break;
-    }
+    //     default:
+    //         filteredTasks = tasks
+    //         break;
+    // }
 
-    if (filteredTasks.length === 0) {
-        return (
-            <>
-                <h1 className="m-3">Liste de tâches</h1>
-                <ul className="list-group m-3">
-                    <li className="list-group-item">Aucune tâche à afficher.</li>
-                </ul>
-            </>
+    // if (filteredTasks.length === 0) {
+    //     return (
+    //         <>
+    //             <h1 className="m-3">Liste de tâches</h1>
+    //             <ul className="list-group m-3">
+    //                 <li className="list-group-item">Aucune tâche à afficher.</li>
+    //             </ul>
+    //         </>
 
-        )
+    //     )
 
-    } else {
-        return (
-            <>
-                <h1 className="m-3">Liste de tâches</h1>
-                <ul className="list-group m-3">
-                    {
-                        filteredTasks.map((task) => <ToDo task={task} key={task.id} onToggleCompleted={onToggleCompleted} />)
-                    }
-                </ul>
-            </>
+    // } else {
+    return (
+        <>
+            <h1 className="m-3">Liste de tâches</h1>
+            <TaskConsumer>
+                {({ tasks }) => (
+                    <ul className="list-group m-3">
+                        {
+                            tasks.map((task) => <ToDo task={task} key={task.id} />)
+                        }
+                    </ul>
 
-        )
+                )}
 
-    }
+            </TaskConsumer>
+        </>
+
+    )
+
+    // }
 }
 
-export default ToDoList
+export default ToDoList;
